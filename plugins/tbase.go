9c27e3f12fbb9dc1d573347cd31ff2c9575b52f2
iminders <liuwen.w@qq.com>
2020-04-07 00:14:55 +0800

    import

--- plugins/tbase.go
+++ plugins/tbase.go
@@ -1,53 +1,54 @@
 package plugins
 
-import(
-    pb "github.com/tradingAI/proto/gen/go/scheduler"
-    "github.com/golang/glog"
-    "strings"
-    "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/golang/glog"
+	pb "github.com/tradingAI/proto/gen/go/scheduler"
 )
 
-type TbasePlugin struct {}
+type TbasePlugin struct{}
 
-func GetTbaseInstallRepoCmds(url string, tag string) (cmds []string){
-    segments := strings.Split(url, "/")
-    name := segments[cap(segments) - 1]
-    cloneCmd := fmt.Sprintf("git clone %s.git", url)
-    checkoutCmd := fmt.Sprintf("git checkout -b %s", tag)
-    cdCmd := fmt.Sprintf("cd %s", name)
-    pipCmd := "pip install -e ."
-    cmds = []string{cloneCmd, cdCmd, checkoutCmd, pipCmd}
-    return
+func GetTbaseInstallRepoCmds(url string, tag string) (cmds []string) {
+	segments := strings.Split(url, "/")
+	name := segments[cap(segments)-1]
+	cloneCmd := fmt.Sprintf("git clone %s.git", url)
+	checkoutCmd := fmt.Sprintf("git checkout -b %s", tag)
+	cdCmd := fmt.Sprintf("cd %s", name)
+	pipCmd := "pip install -e ."
+	cmds = []string{cloneCmd, cdCmd, checkoutCmd, pipCmd}
+	return
 }
 
-func (p *TbasePlugin) GenerateCmds(input *pb.JobInput)(cmds []string, err error){
-    switch input.GetInput().(type) {
-    case *pb.JobInput_EvalInput:
-        // TODO
-        glog.Info("JobInput_EvalInput")
-    case *pb.JobInput_InferInput:
-        // TODO
-        glog.Info("JobInput_InferInput")
-    case *pb.JobInput_TrainInput:
-        // TODO
-        glog.Info("JobInput_TrainInput")
-        return p.getTrainJobCmds(input)
-    }
-    return
+func (p *TbasePlugin) GenerateCmds(input *pb.JobInput) (cmds []string, err error) {
+	switch input.GetInput().(type) {
+	case *pb.JobInput_EvalInput:
+		// TODO
+		glog.Info("JobInput_EvalInput")
+	case *pb.JobInput_InferInput:
+		// TODO
+		glog.Info("JobInput_InferInput")
+	case *pb.JobInput_TrainInput:
+		// TODO
+		glog.Info("JobInput_TrainInput")
+		return p.getTrainJobCmds(input)
+	}
+	return
 }
 
-func (p *TbasePlugin) getTrainJobCmds(input *pb.JobInput)(cmds []string, err error){
-    // add run dir
-    cmds = append(cmds, "mkdir -p /root/runner/")
-    cmds = append(cmds, "cd /root/runner/")
-    // install tenvs dir
-    cmds = append(cmds, input.GetTrainInput().GetTenvsRepo().GetCmds()...)
-    // tbase
-    cmds = append(cmds, "cd /root/runner/")
-    cmds = append(cmds, input.GetTrainInput().GetTbaseRepo().GetCmds()...)
-    // run commands
-    // TODO: 根据parameters参数构造run命令
-    runCmd := fmt.Sprintf("python -m tbase.run --%s %s", "alg", input.GetTrainInput().GetParameters()["alg"])
-    cmds = append(cmds, runCmd)
-    return
+func (p *TbasePlugin) getTrainJobCmds(input *pb.JobInput) (cmds []string, err error) {
+	// add run dir
+	cmds = append(cmds, "mkdir -p /root/runner/")
+	cmds = append(cmds, "cd /root/runner/")
+	// install tenvs dir
+	cmds = append(cmds, input.GetTrainInput().GetTenvsRepo().GetCmds()...)
+	// tbase
+	cmds = append(cmds, "cd /root/runner/")
+	cmds = append(cmds, input.GetTrainInput().GetTbaseRepo().GetCmds()...)
+	// run commands
+	// TODO: 根据parameters参数构造run命令
+	runCmd := fmt.Sprintf("python -m tbase.run --%s %s", "alg", input.GetTrainInput().GetParameters()["alg"])
+	cmds = append(cmds, runCmd)
+	return
 }
--- plugins/tbase_test.go
+++ plugins/tbase_test.go
@@ -1,41 +1,41 @@
 package plugins
 
-import(
-    "testing"
+import (
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetInstallTbaseRepoCmds(t *testing.T){
-    tenvsURL := "https://github.com/tradingAI/tenvs"
-    tenvsTag := "v1.0.3"
-    actual := GetTbaseInstallRepoCmds(tenvsURL, tenvsTag)
-    expected := []string{
-        "git clone https://github.com/tradingAI/tenvs.git",
-        "cd tenvs",
-        "git checkout -b v1.0.3",
-        "pip install -e .",
-    }
-    assert.Equal(t, expected, actual)
+func TestGetInstallTbaseRepoCmds(t *testing.T) {
+	tenvsURL := "https://github.com/tradingAI/tenvs"
+	tenvsTag := "v1.0.3"
+	actual := GetTbaseInstallRepoCmds(tenvsURL, tenvsTag)
+	expected := []string{
+		"git clone https://github.com/tradingAI/tenvs.git",
+		"cd tenvs",
+		"git checkout -b v1.0.3",
+		"pip install -e .",
+	}
+	assert.Equal(t, expected, actual)
 }
 
-func TestGenerateCmds(t *testing.T){
-    p := &TbasePlugin{}
-    input := CreateTestTbaseTrainJobInput()
-    actual, _ := p.GenerateCmds(input)
-    expected := []string{
-        "mkdir -p /root/runner/",
-        "cd /root/runner/",
-        "git clone https://github.com/tradingAI/tenvs.git",
-        "cd tenvs",
-        "git checkout -b v1.0.3",
-        "pip install -e .",
-        "cd /root/runner/",
-        "git clone https://github.com/tradingAI/tbase.git",
-        "cd tbase",
-        "git checkout -b v0.1.2",
-        "pip install -e .",
-        "python -m tbase.run --alg ddpg",
-    }
-    assert.Equal(t, expected, actual)
+func TestGenerateCmds(t *testing.T) {
+	p := &TbasePlugin{}
+	input := CreateTestTbaseTrainJobInput()
+	actual, _ := p.GenerateCmds(input)
+	expected := []string{
+		"mkdir -p /root/runner/",
+		"cd /root/runner/",
+		"git clone https://github.com/tradingAI/tenvs.git",
+		"cd tenvs",
+		"git checkout -b v1.0.3",
+		"pip install -e .",
+		"cd /root/runner/",
+		"git clone https://github.com/tradingAI/tbase.git",
+		"cd tbase",
+		"git checkout -b v0.1.2",
+		"pip install -e .",
+		"python -m tbase.run --alg ddpg",
+	}
+	assert.Equal(t, expected, actual)
 }
